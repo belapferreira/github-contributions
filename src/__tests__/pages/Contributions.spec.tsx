2f5e42cd6be856f838b2a381f3af6db238b8dc2d
belapferreira <isabelapenhaferreira@gmail.com>
2020-12-13 18:12:02 -0300

    chore: add test for select year element

--- src/__tests__/pages/Contributions.spec.tsx
+++ src/__tests__/pages/Contributions.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 
 import Contributions from '../../pages/Contributions';
 
@@ -8,5 +8,17 @@ describe('Contributions Page', () => {
     const { debug } = render(<Contributions />);
 
     debug();
+  });
+
+  it('should be able to switch year on change', async () => {
+    const { getByTestId } = render(<Contributions />);
+
+    const select = getByTestId('select');
+
+    fireEvent.change(select, { target: { value: '2016' } });
+
+    await waitFor(() => {
+      expect(select).toHaveTextContent('2016');
+    });
   });
 });
